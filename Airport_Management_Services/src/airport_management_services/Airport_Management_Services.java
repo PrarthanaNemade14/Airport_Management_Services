70d4dcec6bd31917c0f777db91fc174e7c1e15cc
PrarthanaNemade14 <nemade.pr@northeastern.edu>
2022-12-07 16:26:39 -0500

    Workspace commit

---
+++ Airport_Management_Services/src/airport_management_services/Airport_Management_Services.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package airport_management_services;
+
+/**
+ *
+ * @author nemad
+ */
+public class Airport_Management_Services {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
