a276f95ca5e9b45e89732a19dc532ef07502d536
MamthaShetty27 <114687465+MamthaShetty27@users.noreply.github.com>
2022-12-07 17:02:57 -0500

    initial commit

---
+++ AirlineManagementSystem/src/model/User.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.List;
+
+/**
+ *
+ * @author mamthashetty
+ */
+public class User {
+    private String username;
+    private String name;
+    private String email;
+    private String age;
+    private String gender;
+    private String address;
+    private String postalCode;
+    private String city;
+    private String state;
+    //private List<Order> orders;
+    //private List<RestaurantOrder> restaurantOrders;
+    
+}
